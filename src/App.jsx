27ac71f37aa78cab27099901c14fc80cf861e776
shashank <molugushashank@gmail.com>
2024-05-05 12:13:32 +0530

    1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-function App() {
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
 
+const App = () => {
   return (
     <>
-      
+      <Sidebar />
+      <Main />
     </>
   )
 }
